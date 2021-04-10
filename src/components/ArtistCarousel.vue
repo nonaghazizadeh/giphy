01b4e53eb44cdcde8f058a858915c337b58c00ba
nonaghazizadeh <nonaghazizadeh@yahoo.com>
2021-04-10 19:02:24 +0430

    slideshow for trending and artists done / fetch data from api

--- src/components/ArtistCarousel.vue
+++ src/components/ArtistCarousel.vue
@@ -10,31 +10,22 @@
     >
     <b-carousel-slide>
     <template v-slot:img>
-      <b-card-group deck>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=54" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=58" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
+      <b-card-group deck >
+        <b-card v-for="source in artistsSources.slice(0,4)" :key="source" :img-src="source"></b-card>
       </b-card-group>
     </template>
     </b-carousel-slide>
     <b-carousel-slide>
     <template v-slot:img>
       <b-card-group deck>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=54" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=58" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
+          <b-card v-for="source in artistsSources.slice(4,8)" :key="source" :img-src="source"></b-card>
       </b-card-group>
     </template>
     </b-carousel-slide>
     <b-carousel-slide>
     <template v-slot:img>
       <b-card-group deck>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=54" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=58" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
+        <b-card v-for="source in artistsSources.slice(8,12)" :key="source" :img-src="source"></b-card>
       </b-card-group>
     </template>
     </b-carousel-slide>
@@ -43,12 +34,17 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
   export default {
     name: 'Carousel',
     data() {
       return {
         slide: 0,
-        sliding: null
+        sliding: null,
+        artistsSources: []
       }
     },
     methods: {
@@ -58,8 +54,19 @@
       onSlideEnd() {
         this.sliding = false
       }
+    },
+    mounted(){
+        let APIKEY = "SJNLdIKsz1131URk7ADVxs7gKcKTRW6z";
+        let artistStr = "artists";
+        let sourceURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=${artistStr}`;
+        Vue.axios.get(sourceURL)
+        .then(response => {
+            for (let i=0 ; i <response.data.data.length ; i ++){
+                this.artistsSources.push(response.data.data[i].images.original.url)
+            }
+        })
     }
-  }
+}
 </script>
 
 <style scoped>
@@ -68,8 +75,15 @@
     margin-right:0;
     margin-left: 5px
     }
+    .card-deck{
+        height:320px
+    }
 }
 .card{
   border: 1px solid black
+}
+img{
+    height:100%;
+    position: absolute;
 }
 </style>
--- src/components/TrendingCarousel.vue
+++ src/components/TrendingCarousel.vue
@@ -10,31 +10,22 @@
     >
     <b-carousel-slide>
     <template v-slot:img>
-      <b-card-group deck class="height">
-        <b-card class="inner" img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=54" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=58" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
+      <b-card-group deck>
+        <b-card v-for="source in trendingSources.slice(0,4)" :key="source" :img-src="source"></b-card>
       </b-card-group>
     </template>
     </b-carousel-slide>
     <b-carousel-slide>
     <template v-slot:img>
       <b-card-group deck>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=54" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=58" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
+        <b-card v-for="source in trendingSources.slice(4,8)" :key="source" :img-src="source"></b-card>
       </b-card-group>
     </template>
     </b-carousel-slide>
     <b-carousel-slide>
     <template v-slot:img>
       <b-card-group deck>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=54" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=58" overlay></b-card>
-        <b-card img-src="https://picsum.photos/1024/480/?image=52" overlay></b-card>
+        <b-card v-for="source in trendingSources.slice(8,12)" :key="source" :img-src="source"></b-card>
       </b-card-group>
     </template>
     </b-carousel-slide>
@@ -43,12 +34,17 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
   export default {
     name: 'Carousel',
     data() {
       return {
         slide: 0,
-        sliding: null
+        sliding: null,
+        trendingSources: []
       }
     },
     methods: {
@@ -58,6 +54,16 @@
       onSlideEnd() {
         this.sliding = false
       }
+    },
+        mounted(){
+        let APIKEY = "SJNLdIKsz1131URk7ADVxs7gKcKTRW6z";
+        let trendingURL = `https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=12`;
+        Vue.axios.get(trendingURL)
+        .then(response => {
+            for (let i=0 ; i <response.data.data.length ; i ++){
+                this.trendingSources.push(response.data.data[i].images.original.url)
+            }
+        })
     }
   }
 </script>
@@ -68,9 +74,16 @@
   margin-right:0;
   margin-left: 5px
   }
+  .card-deck{
+        height:200px
+    }
 }
 .card{
   border: 1px solid black
+}
+img{
+  height:100%;
+  position: absolute;
 }
 
 </style>
