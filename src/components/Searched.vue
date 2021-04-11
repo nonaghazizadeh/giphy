82077a9717bc0d93d8a6d3b118ad1a7ffab3ffd5
nonaghazizadeh <nonaghazizadeh@yahoo.com>
2021-04-11 12:51:24 +0430

    search fixed

--- src/components/Searched.vue
+++ src/components/Searched.vue
@@ -1,5 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <b-container fluid class="p-4">
+      <b-row>
+        <b-col>
+          <b-img
+            class="mb-4"
+            fluid
+            v-for="source in searchedList.slice(0, 15)"
+            :key="source"
+            :src="source"
+          ></b-img>
+        </b-col>
+        <b-col>
+          <b-img
+            class="mb-4"
+            fluid
+            v-for="source in searchedList.slice(15, 30)"
+            :key="source"
+            :src="source"
+          ></b-img>
+        </b-col>
+        <b-col>
+          <b-img
+            class="mb-4"
+            fluid
+            v-for="source in searchedList.slice(30, 45)"
+            :key="source"
+            :src="source"
+          ></b-img>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
@@ -10,41 +42,41 @@ Vue.use(VueAxios, axios);
 export default {
   name: "Searched",
   props: ["searchInput"],
-  data(){
-      return{
-          searchedList:[]
-      }
+  data() {
+    return {
+      searchedList: [],
+    };
+  },
+  watch: {
+    searchInput: {
+      handler(newValue) {
+        let APIKEY = "SJNLdIKsz1131URk7ADVxs7gKcKTRW6z";
+        let searchStr = newValue;
+        let sourceURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=45&q=${searchStr}`;
+        Vue.axios.get(sourceURL).then((response) => {
+          for (let i = 0; i < response.data.data.length; i++) {
+            this.searchedList.push(response.data.data[i].images.original.url);
+          }
+        });
+        console.log(this.searchedList);
+      },
+    },
   },
   mounted() {
     let APIKEY = "SJNLdIKsz1131URk7ADVxs7gKcKTRW6z";
     let searchStr = this.searchInput;
-    let sourceURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=${searchStr}`;
+    let sourceURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=45&q=${searchStr}`;
     Vue.axios.get(sourceURL).then((response) => {
       for (let i = 0; i < response.data.data.length; i++) {
         this.searchedList.push(response.data.data[i].images.original.url);
       }
     });
-    console.log(this.searchedList)
-
   },
 };
 </script>
 
 <style scoped>
-@media (min-width: 576px) {
-  .card-deck .card {
-    margin-right: 0;
-    margin-left: 5px;
-  }
-  .card-deck {
-    height: 200px;
-  }
-}
-.card {
-  border: 1px solid black;
-}
-img {
-  height: 100%;
-  position: absolute;
+.col {
+  text-align: center;
 }
 </style>
